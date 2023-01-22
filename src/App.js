7587d1a7f074658f94fc21250bd8f003aea48915
Bayardo <bayarloper@gmail.com>
2023-01-22 13:14:17 +0800

    Header and Another Section added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import './App.css';
-import Navbar from './Components/Navbar';
-import Slider from './Components/Slider';
-import Services from './Components/Services';
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import Slider from "./Components/Slider/Slider";
+import Services from "./Components/Services/Services";
+import Sidebar from "./Components/Sidebar/Sidebar";
+import Calculation from "./Components/Calculation/Calculation";
 
 function App() {
   return (
     <>
-      <div className='main-container'>
+      <div className="container">
         <Navbar></Navbar>
         <Slider></Slider>
-        <Services></Services>
+        <div className="row">
+          <div className="col-sm-8">
+            <Services />
+          </div>
+          <div className="col-sm-4">
+            <Sidebar />
+          </div>
+        </div>
+        <div>
+          <Calculation />
+        </div>
       </div>
     </>
   );
